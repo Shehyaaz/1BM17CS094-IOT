bc79b324dc56f59fbbc631b8fe631395208b6d9d
Shehyaaz <49763208+Shehyaaz@users.noreply.github.com>
2019-08-26 16:01:02 +0530

    completed light dependent resistor experiment

---
+++ light_dependent_resistor.ino
@@ -0,0 +1,21 @@
+int ledPin = 13;
+int analogPin = A0;
+int threshold = 500;
+
+void setup(){
+  pinMode(ledPin,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  int sensedValue = analogRead(analogPin);
+  Serial.print("Read Value :");
+  Serial.println(sensedValue);
+  if(sensedValue < threshold){
+    digitalWrite(ledPin,HIGH);
+    delay(1000);
+  }
+  else{
+    digitalWrite(ledPin,LOW);
+  }
+}
